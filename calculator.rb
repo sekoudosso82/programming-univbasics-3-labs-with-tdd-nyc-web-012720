d7d7706ec47408be36bf0b82dda9cb48482d4018
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-01 23:43:32 +0000

    Done.

--- calculator.rb
+++ calculator.rb
@@ -1,1 +1,10 @@
-# Add your variables here
+# first_number = get_variable_from_file
+first_number = 2 
+second_number  = 3
+
+sum = first_number+second_number
+difference = first_number - second_number 
+product = first_number * second_number
+quotient = first_number/second_number
+
+
