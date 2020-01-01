3471314813aa8c2fbb1aabef2dba46b72b4d3584
Sekou Dosso <sekou.dosso82@gmail.com>
2020-01-01 23:43:16 +0000

    Automatically backed up by Learn

--- calculator.rb
+++ calculator.rb
@@ -1,10 +1,10 @@
 # first_number = get_variable_from_file
-var first_number = 2 
-var second_number  = 3
+first_number = 2 
+second_number  = 3
 
-var sum = first_number+second_number
-var difference = first_number - second_number 
-var product = first_number * second_number
-var quotient = first_number/second_number
+sum = first_number+second_number
+difference = first_number - second_number 
+product = first_number * second_number
+quotient = first_number/second_number
 
 
